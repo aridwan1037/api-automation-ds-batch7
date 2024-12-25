809223ab5989082fe77e64963e5957cefa7aecb2
aridwan1037 <aridwan1037@gmail.com>
2024-12-25 16:54:27 +0700

    minor perpihan code api-test.spec.js

--- tests/api-test.spec.js
+++ tests/api-test.spec.js
@@ -2,28 +2,30 @@ const { test, expect } = require("@playwright/test");
 const { Ajv } = require("ajv");
 const { log, error } = require("console");
 
-const ajv = new Ajv()
+const ajv = new Ajv();
 
 test("Tugas GET", async ({ request }) => {
   const response = await request.get("https://reqres.in/api/users/2");
-  const responseData = await response.json()
-  const valid = ajv.validate(require('./json-schema/get-singleObject-schema.json'), responseData)
+  const responseData = await response.json();
+  const valid = ajv.validate(
+    require("./json-schema/get-singleObject-schema.json"),
+    responseData
+  );
 
   //assertion
-  expect(response.status()).toBe(200)
-  expect(responseData.data.email).toBe("janet.weaver@reqres.in")
-  expect(valid).toBe(true)  
+  expect(response.status()).toBe(200);
+  expect(responseData.data.email).toBe("janet.weaver@reqres.in");
+  expect(valid).toBe(true);
 
-  if(!valid){
-    console.error("AJV validation Errors: ",ajv.errorsText())
+  if (!valid) {
+    console.error("AJV validation Errors: ", ajv.errorsText());
   }
-
 });
 
 test("Tugas POST", async ({ request }) => {
   const bodyData = {
-    "name": "morpheus",
-    "job": "leader"
+    name: "morpheus",
+    job: "leader",
   };
 
   const headerData = {
@@ -34,73 +36,55 @@ test("Tugas POST", async ({ request }) => {
     headers: headerData,
     data: bodyData,
   });
-  const responseData = await response.json()
-  const valid = ajv.validate(require('./json-schema/post-createUser-schema.json'), responseData)
+  const responseData = await response.json();
+  const valid = ajv.validate(
+    require("./json-schema/post-createUser-schema.json"),
+    responseData
+  );
 
   //assertion
-  expect(response.status()).toBe(201)
-  expect(responseData).toHaveProperty('id')
-  expect(valid).toBe(true)
+  expect(response.status()).toBe(201);
+  expect(responseData).toHaveProperty("id");
+  expect(valid).toBe(true);
 
-  if(!valid){
-    console.error("AJV validation Errors: ",ajv.errorsText())
+  if (!valid) {
+    console.error("AJV validation Errors: ", ajv.errorsText());
   }
-
 });
 
-test('Tugas DELETE', async ({ request }) => {
-    const response = await request.delete("https://reqres.in/api/users/2")
+test("Tugas DELETE", async ({ request }) => {
+  const response = await request.delete("https://reqres.in/api/users/2");
 
   //assertion
-  expect(response.status()).toBe(204)
-    
+  expect(response.status()).toBe(204);
 });
 
 test("Tugas PUT", async ({ request }) => {
-    const bodyData = {
-      "name": "morpheus",
-      "job": "zion resident"
-    };
-  
-    const headerData = {
-      Accept: "application/json",
-    };
-  
-    const response = await request.put("https://reqres.in/api/users/2", {
-      headers: headerData,
-      data: bodyData,
-    });
-    const responseData = await response.json()
-    const valid = ajv.validate(require('./json-schema/put-update-schema.json'), responseData)
-  
-    //assertion
-    expect(response.status()).toBe(200)
-    expect(responseData.job).toBe('zion resident')
-    expect(valid).toBe(true)
-  
-    if(!valid){
-      console.error("AJV validation Errors: ",ajv.errorsText())
-    }
-  
-  });
+  const bodyData = {
+    name: "morpheus",
+    job: "zion resident",
+  };
 
-/*
-test.describe("Positif Test Cases", () => {
-  //bisa juga dituliskan seperti dibawah tanpa async {}
-  test("Test Case 2", () => {
-    console.log("Dieksekusi dari positif 2");
-  });
-});
+  const headerData = {
+    Accept: "application/json",
+  };
 
-test.describe("Negative Test Cases", () => {
-  test("Test Case 3", async ({}) => {
-    console.log("Dieksekusi dari negatif 1");
+  const response = await request.put("https://reqres.in/api/users/2", {
+    headers: headerData,
+    data: bodyData,
   });
-  test("Test Case 4", async ({}) => {
-    console.log("Dieksekusi dari negatif 1");
-  });
-  test("Test Case 5", async ({}) => {
-    console.log("Dieksekusi dari negatif 1");
-  });
+  const responseData = await response.json();
+  const valid = ajv.validate(
+    require("./json-schema/put-update-schema.json"),
+    responseData
+  );
+
+  //assertion
+  expect(response.status()).toBe(200);
+  expect(responseData.job).toBe("zion resident");
+  expect(valid).toBe(true);
+
+  if (!valid) {
+    console.error("AJV validation Errors: ", ajv.errorsText());
+  }
 });
-*/
